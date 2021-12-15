9611b55d135b2f35982977b5af18b337af274952
Narinder Singh Punn <40822259+nspunn1993@users.noreply.github.com>
2021-12-15 16:07:38 +0530

    Update BT-Unet.py

--- BT-Unet.py
+++ BT-Unet.py
@@ -381,6 +381,77 @@ barlow_twins.encoder.load_weights("barlow_twins_UNET")
 
 
 ################### METRICS ###################
+def iou_metric(y_true_in, y_pred_in, print_table=False):
+    labels = label(y_true_in > 0.5)
+    y_pred = label(y_pred_in > 0.5)
+    
+    true_objects = len(np.unique(labels))
+    pred_objects = len(np.unique(y_pred))
+
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
+
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins = true_objects)[0]
+    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1)
+    area_pred = np.expand_dims(area_pred, 0)
+
+    # Compute union
+    union = area_true + area_pred - intersection
+
+    # Exclude background from the analysis
+    intersection = intersection[1:,1:]
+    union = union[1:,1:]
+    union[union == 0] = 1e-9
+
+    # Compute the intersection over union
+    iou = intersection / union
+    #print('IOU {}'.format(iou))
+    # Precision helper function
+    def precision_at(threshold, iou):
+        matches = iou > threshold
+        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
+        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
+        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
+        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
+        return tp, fp, fn
+
+    # Loop over IoU thresholds
+    prec = []
+    if print_table:
+        print("Thresh\tTP\tFP\tFN\tPrec.")
+    for t in np.arange(0.5, 1.0, 0.05):
+        tp, fp, fn = precision_at(t, iou)
+        if (tp + fp + fn) > 0:
+            p = tp / (tp + fp + fn)
+        else:
+            p = 0
+        if print_table:
+            print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(t, tp, fp, fn, p))
+        prec.append(p)
+    
+    if print_table:
+        print("AP\t-\t-\t-\t{:1.3f}".format(np.mean(prec)))
+    return np.mean(prec)
+
+def iou_metric_batch(y_true_in, y_pred_in):
+    batch_size = y_true_in.shape[0]
+    value = 0.
+    for batch in range(batch_size):
+        value = value + iou_metric(y_true_in[batch], y_pred_in[batch])
+    return value/batch_size
+
+def my_iou_metric(label, pred):
+    metric_value = tf.py_function(iou_metric_batch, [label, pred], tf.float32)
+    return metric_value
+
+def my_iou_metric_loss(label, pred):
+    loss = 1-tf.py_function(iou_metric_batch, [label, pred], tf.float32)
+    #loss = -tf.map_fn(my_iou_metric_loss(label, pred), tf.range(tf.shape(pred)[0]))
+    loss.set_shape((None,))
+
+    return loss
+
 def dice_coeff(y_true, y_pred):
     smooth = 1.
     y_true_f = K.flatten(y_true)
@@ -436,11 +507,12 @@ def evalResult(gt,pred,target_size=(256,256),flag_multi_class = False,num_class 
     
     hd = haud_dist_batch(gt,pred)
     
+    miou = iou_metric_batch(gt,pred)
+    
     mae = MeanAbsoluteError()
     r_mae = mae(np.squeeze(gt), np.squeeze(pred)).numpy()
 
-    print("Accuracy=",r_acc, "Precision=",r_pr, "Recall=",r_rc, "MeanIoU=",r_mi, "DiceCoefficient=",dc, "HD=",hd, "MAE=",r_mae)
-
+    print("Accuracy=",r_acc, "Precision=",r_pr, "Recall=",r_rc, "MeanIoU=",r_mi, "DiceCoefficient=",dc, "HD=",hd, "MyIoU=",miou, "MAE=",r_mae)
 
 ################### LOSS FUNCTIONS ###################
 def focal_loss(target_tensor, prediction_tensor, weights=None, alpha=0.25, gamma=2):
